a26e6b6ee11d9baae763f52c1a1b99feea3b3522
erikseptian09 <121048520+erikseptian09@users.noreply.github.com>
2025-05-25 19:44:20 +0700

    Update form.js

--- form.js
+++ form.js
@@ -1,17 +1,5 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyD-6bKBL82Gyc_yVYn8AyLmK3ih3IiFtpI",
-  authDomain: "aplikasi-pelanggan.firebaseapp.com",
-  databaseURL: "https://aplikasi-pelanggan-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "aplikasi-pelanggan",
-  storageBucket: "aplikasi-pelanggan.firebasestorage.app",
-  messagingSenderId: "693408241938",
-  appId: "1:693408241938:web:8c1099bb15533056f6bbcb",
-  measurementId: "G-4E8QYEE8ES"
-};
 
-firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
-const db = firebase.database();
 
 // Show/Hide Password
 function togglePassword() {
