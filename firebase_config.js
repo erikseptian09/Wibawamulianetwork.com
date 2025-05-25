e7300ca8be739ee87c6d53ab3678261b0007b391
erikseptian09 <121048520+erikseptian09@users.noreply.github.com>
2025-05-26 03:15:56 +0700

    Update firebase_config.js

--- firebase_config.js
+++ firebase_config.js
@@ -1,12 +1,14 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyBXnXCSXhy06eqWBPzYmzxCo_LBzl51DO4",
-    authDomain: "wibawamulianetwork.firebaseapp.com",
-    projectId: "wibawamulianetwork",
-    storageBucket: "wibawamulianetwork.firebasestorage.app",
-    messagingSenderId: "605020393823",
-    appId: "1:605020393823:web:6ba8afe7e250bc004da6ea",
-    measurementId: "G-9EWJE8F5FG"
-  };
+  apiKey: "AIzaSyBXnXCSXhy06eqWBPzYmzxCo_LBzl51DO4",
+  authDomain: "wibawamulianetwork.firebaseapp.com",
+  databaseURL: "https://wibawamulianetwork-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "wibawamulianetwork",
+  storageBucket: "wibawamulianetwork.firebasestorage.app",
+  messagingSenderId: "605020393823",
+  appId: "1:605020393823:web:6ba8afe7e250bc004da6ea",
+  measurementId: "G-9EWJE8F5FG"
+};
 
 firebase.initializeApp(firebaseConfig);
